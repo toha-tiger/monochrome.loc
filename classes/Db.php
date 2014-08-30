4b5b981d570d6f28c89ed9543bce4ee345079604
Toha Tiger <toha.tiger@gmail.com>
2014-08-30 12:47:44 +0300

    improve DB class

--- classes/Db.php
+++ classes/Db.php
@@ -17,4 +17,49 @@ class Db extends Config {
             die('Db error');
         }
     }
+
+    //query('SELECT login FROM WHERE id = ?' , array($id))
+    //or
+    //query('SELECT login FROM WHERE id = :id' , array(':id' => $id))
+    function query($query, $params = array()) {
+        $stmt = $this->db->prepare($query);
+        if (!empty($params))
+        {
+            $i = 0; 
+            foreach ($params as $param_name => $param_value) {
+                if (is_numeric($param_name)) {
+                    $stmt->bindValue($i, $param_value);
+                } else {
+                    $stmt->bindParam($param_name, $param_value);
+                }
+                $i++;
+            }
+        }
+        try {
+            if ($stmt->execute()) {
+                return $stmt->fetchAll(PDO::FETCH_OBJ);
+            }
+        } catch (PDOException $e) {
+            $this->errors[] = 'Error ';
+            if(self::DEBUG_MODE) {
+                $this->errors[] = $e->getMessage();
+            }
+            error_log ('Db->query error ' . $e->getMessage());
+        }
+        return false;
+    }
+
+    //get('users', array('login', '=', 'test'))
+    function get($table, $where = array()) {
+        if (count($where) == 3)
+        {
+            $field = $where[0];
+            $operator = $where[1];
+            $value = $where[2];
+            $query = "SELECT * FROM {$table} WHERE {$field} {$operator} ?";
+            $result = $this->query($query, array($value));
+            
+        }
+
+    }
 }
