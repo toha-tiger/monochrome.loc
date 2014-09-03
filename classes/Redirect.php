7d943b4802a1cabcac99d9f4e0e83bcd8359700b
Toha Tiger <toha.tiger@gmail.com>
2014-09-03 20:45:38 +0300

    404 page

--- classes/Redirect.php
+++ classes/Redirect.php
@@ -5,5 +5,9 @@ class Redirect {
         header("Location: " . lib::make_link($page));
     }
 
+    public static function page404(){
+        header("HTTP/1.0 404 Not Found");
+        die('404. This is not the page you\'re looking for');
+    }
 
 } 
--- index.php
+++ index.php
@@ -3,8 +3,7 @@ require 'classes/init.php';
 
 //router
 $page = lib::get_var('page');
-if (empty($page))
-{
+if (empty($page)) {
     $page = 'index';
 }
 $page_file = "pages/Page_${page}.php";
@@ -15,12 +14,14 @@ if (file_exists($page_file))
     $page_class = "Page_$page";
     $page = new $page_class;
 } else {
-    die("Page_$page doesn't exists");
+    Redirect::page404();
 }
 $action = lib::get_var('action');
-if (empty($action))
-{
+if (empty($action)) {
     $action = 'show';
+}
+if (!method_exists($page, $action)) {
+    Redirect::page404();
 }
 $page->$action();
 //end router
